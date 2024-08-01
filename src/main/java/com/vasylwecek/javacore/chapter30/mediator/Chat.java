5be551c4eccd48981c890214c8681e21842ebf20
Vasyl <vasylwecek@gmail.com>
2024-08-01 15:37:11 +0200

    Mediator

---
+++ src/main/java/com/vasylwecek/javacore/chapter30/mediator/Chat.java
@@ -0,0 +1,5 @@
+package chapter30.mediator;
+
+public interface Chat {
+    public void sendMessage(String message, User user);
+}
---
+++ src/main/java/com/vasylwecek/javacore/chapter30/mediator/User.java
@@ -0,0 +1,7 @@
+package chapter30.mediator;
+
+public interface User {
+    public void sendMessage(String message);
+    public void getMessage(String message);
+
+}
