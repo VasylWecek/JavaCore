448d2510aeed7348d4fd97e1a124d2bba6b21ffe
Vasyl <vasylwecek@gmail.com>
2024-08-01 16:52:16 +0200

    Strategy

---
+++ src/main/java/com/vasylwecek/javacore/chapter30/strategy/Activity.java
@@ -0,0 +1,5 @@
+package chapter30.strategy;
+
+public interface Activity {
+    public void justDoIt();
+}
---
+++ src/main/java/com/vasylwecek/javacore/chapter30/strategy/Training.java
@@ -0,0 +1,8 @@
+package chapter30.strategy;
+
+public class Training implements Activity{
+    @Override
+    public void justDoIt() {
+        System.out.println("Trainig...");
+    }
+}
